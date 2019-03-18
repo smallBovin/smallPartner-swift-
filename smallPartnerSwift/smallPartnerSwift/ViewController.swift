41d445f258998fe838295672812e5467fcb59e34
李保洋 <bovin150@gmail.com>
2019-03-18 10:14:25 +0800

    初始化空白工程

---
+++ smallPartnerSwift/smallPartnerSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smallPartnerSwift
+//
+//  Created by 李保洋 on 2019/3/18.
+//  Copyright © 2019 bovin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
